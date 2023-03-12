a937050f1cd967f8dc9a269d5bf66808c4a6aaa0
Anurag <89177393+xyrocodes@users.noreply.github.com>
2023-03-12 09:27:59 +0000

    fifth commit

--- index.js
+++ index.js
@@ -9,21 +9,28 @@ const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 const MongoStore = require('connect-mongo')(session);
-// const sassMiddleware =require('node-sass-middleware');
+const sassMiddleware = require('node-sass-middleware');
+const flash = require('connect-flash');
+const customMware = require('./config/middleware');
 
 
-// app.use(sassMiddleware({
-//     src: './assets/scss',
-//     dest: './assets/css',
-//     debug: true,
-//     outputStyle: 'extended',
-//     prefix: '/css'
-// }));
+app.use(sassMiddleware({
+    src: './assets/scss',
+    dest: './assets/css',
+    debug: true,
+    outputStyle: 'extended',
+    prefix: '/css'
+}));
+
+
 app.use(express.urlencoded());
 
 app.use(cookieParser());
 
 app.use(express.static('./assets'));
+
+// make the uploads path available to the browser
+app.use('/uploads', express.static(__dirname + '/uploads'));
 
 app.use(expressLayouts);
 // extract style and scripts from sub pages into the layout
@@ -63,6 +70,9 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(passport.setAuthenticatedUser);
+
+app.use(flash());
+app.use(customMware.setFlash);
 
 // use express router
 app.use('/', require('./routes'));
--- models/user.js
+++ models/user.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
 
+// importing multer
+const multer = require('multer');
+const path = require('path');
+const AVATAR_PATH = path.join('/uploads/users/avatars');
+
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
@@ -13,10 +18,28 @@ const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
+    },
+    avatar: {
+        type: String
     }
 }, {
     timestamps: true
 });
+
+
+
+let storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, path.join(__dirname, '..', AVATAR_PATH));
+    },
+    filename: function(req, file, cb){
+        cb(null, file.fieldname + '-' + Date.now())
+    }
+});
+
+// static functions
+userSchema.statics.uploadedAvatar = multer({storage: storage}).single('avatar');
+userSchema.static.avatarPath = AVATAR_PATH;
 
 
 const User = mongoose.model('User', userSchema);
